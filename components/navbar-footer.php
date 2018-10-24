c7dbb68a1d9275b7d4400a39ccbf4879e82568b2
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:18:41 +0200

    footer: put navbar in a component

---
+++ components/navbar-footer.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#SecondNavbar" aria-controls="SecondNavbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="SecondNavbar">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item inactive">
+        CC BY-SA 2018 <strong>Quarante-Douze</strong>
+      </li>
+    </ul>
+  
+    <ul class="navbar-nav my-2 my-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-fw fa-rss"></i></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-fw fa-twitter"></i></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-fw fa-facebook"></i></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-fw fa-diaspora"></i></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fa fa-fw fa-mastodon"></i></a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
