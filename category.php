a9c54dba0ce3aee4f9add4a536f2f07531749834
Kazhnuz <antoine@sortilege.net>
2018-10-24 21:45:47 +0200

    category: add the RSS link

--- category.php
+++ category.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?> <!-- ouvrir header,php -->
 <main class="col-md-8">
-  <h1 class="page-title">
-    <i class="fa fa-fw fa-folder"></i> <?php echo single_cat_title(); ?>
+  <h1 class="page-title page-title-flex">
+    <span><i class="fa fa-fw fa-folder"></i> <?php echo single_cat_title(); ?></span>
+    <?php 
+      $category = get_category( get_query_var( 'cat' ) );
+      $cat_id = $category->cat_ID;
+    
+      echo '<a href="' . get_category_link( $cat_id ) . 'feed/" title="RSS de la categorie" />' . '<i class="fa fa-fw fa-rss"></i></a>' ; 
+    ?>
   </h1>
 
   <?php include(TEMPLATEPATH . '/components/posts-list.php'); ?>
