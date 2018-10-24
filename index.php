510f62452bbbe6f0bd4b14bdc5a6b4da6e780009
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:32:25 +0200

    index: separate the sidebar

--- index.php
+++ index.php
@@ -174,55 +174,5 @@ In id suscipit elit.</code></pre>
 </article>
 
 </div>
-<aside class="sidebar col-md-3">
-  <section class="card card-primary">
-    <div class="card-header">
-      <h3 class="card-title">Catégories</h3>
-    </div>
-    <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-        Objet de liste
-        <span class="badge badge-primary badge-pill">14</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-    </div>
-  </section>
-
-  <section class="card card-primary">
-    <div class="card-header">
-      <h3 class="card-title"> Archive</h3>
-    </div>
-    <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-        Objet de liste
-        <span class="badge badge-primary badge-pill">14</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-    </div>
-  </section>
-
-  <section class="card card-primary">
-      <div class="card-header">
-        <h3 class="card-title">Navigation</h3>
-      </div>
-      <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-        Objet de liste
-        <span class="badge badge-primary badge-pill">14</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
-    </div>
-  </section>
-
-
-</aside>
-<?php get_footer(); ?>
+<?php get_sidebar(); ?>
+<?php get_footer();  ?>
---
+++ sidebar.php
@@ -0,0 +1,51 @@
+<aside class="sidebar col-md-3">
+  <section class="card card-primary">
+    <div class="card-header">
+      <h3 class="card-title">Catégories</h3>
+    </div>
+    <div class="list-group list-group-flush">
+      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+        Objet de liste
+        <span class="badge badge-primary badge-pill">14</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+    </div>
+  </section>
+
+  <section class="card card-primary">
+    <div class="card-header">
+      <h3 class="card-title"> Archive</h3>
+    </div>
+    <div class="list-group list-group-flush">
+      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+        Objet de liste
+        <span class="badge badge-primary badge-pill">14</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+    </div>
+  </section>
+
+  <section class="card card-primary">
+      <div class="card-header">
+        <h3 class="card-title">Navigation</h3>
+      </div>
+      <div class="list-group list-group-flush">
+      <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+        Objet de liste
+        <span class="badge badge-primary badge-pill">14</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+      <a href="#" class="list-group-item list-group-item-action">Objet de liste</a>
+    </div>
+  </section>
+
+
+</aside>
