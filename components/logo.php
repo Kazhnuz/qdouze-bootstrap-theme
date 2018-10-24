d540cee847d2084684003385b5de775f53371dd5
Kazhnuz <antoine@sortilege.net>
2018-10-24 21:12:43 +0200

    header components: use site_url to redirect to the site url
    Fix #19

--- components/logo.php
+++ components/logo.php
@@ -1,3 +1,3 @@
 <section class="container-fluid">
-  <h1><a href="https://quarante-douze.net/"><img src="<?php echo get_template_directory_uri();?>/img/logo.png"></a></h1>
+  <h1><a href="<?php echo site_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/img/logo.png"></a></h1>
 </section>
--- components/navbar-pages.php
+++ components/navbar-pages.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-  <a class="navbar-brand" href="#" id="" ><img src="<?php echo get_template_directory_uri();?>/img/brand-icon.png" alt="Quarante-Douze"></a>
+  <a class="navbar-brand" href="<?php echo site_url(); ?>" id="" ><img src="<?php echo get_template_directory_uri();?>/img/brand-icon.png" alt="Quarante-Douze"></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
