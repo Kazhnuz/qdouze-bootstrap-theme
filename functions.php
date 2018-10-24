e2cab60a0532459e63a7a1fa3e709146841050b4
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:35:25 +0200

    sidebar: initial dynamic sidebar support
    Doesn't work yet as I haven't put wp_footer in the footer

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+/* function.php
+ *
+ * All the function of Quarante-Douze
+ *
+ */
+ 
+
+/* 1. Sidebar support */
+
+  if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+        'before_widget' => '<section id="%1$s" class="widget %2$s card card-secondary">',
+        'after_widget' => '</section>',
+        'before_title' => '<h1 class="section card-header">',
+        'after_title' => '</h1>',
+    ));
