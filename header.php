d3b75e0f56de680453645df7c3d6cdaa7fcde2fa
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:02:44 +0200

    Put the header in header.php

---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Blue Sky, by Kazhnuz</title>
+
+		<!-- meta -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<!-- mon icon -->
+		<link rel="shortcut icon" href="img/favicon.png">
+
+		<!-- framework utilisés -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/fork-awesome.css" rel="stylesheet">
+
+		<link href="style.css" rel="stylesheet">
+	</head>
+
+	<body>
+<div id="wrapper" class="card">
+
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#" id="" ><img src="img/brand-icon.png" alt="Quarante-Douze"></a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">À propos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+
+  <section class="container-fluid">
+		<h1><a href="https://quarante-douze.net/"><img src="img/logo.png"></a></h1>
+  </section>
+	
+<nav class="navbar navbar-expand-lg navbar-dark bg-grey" id="navbar-category">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav nav-justified w-100">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">La vie du blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contenus Culturels</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fandoms</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tribunes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Libreries</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Internet et Numérique</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</header>
+
+<section class="container-fluid">
+	<div class="row">
