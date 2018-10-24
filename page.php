bb78235d6de3b88b8f10d4d58bd56bb6a7a02e44
Kazhnuz <antoine@sortilege.net>
2018-10-24 17:18:28 +0200

    page: add basic page handling

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?> <!-- ouvrir header,php -->
+<main class="col-md-8">
+  <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+  
+  <article class="article-content" id="post-<?php the_ID(); ?>">
+  <h1 class="page-title"><?php the_title(); ?></h1>
+    
+    <div class="article-body">
+      <?php the_content(); ?>
+    </div>
+  </article>
+  
+  <?php endwhile; ?>
+  <?php endif; ?>
+</main>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
