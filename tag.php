5745bf904281cd649a0e72b27f40c5cc2df993ae
Kazhnuz <antoine@sortilege.net>
2018-10-24 21:50:29 +0200

    tag: add the RSS link
    Fix #17

--- tag.php
+++ tag.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?> <!-- ouvrir header,php -->
 <main class="col-md-8">
-    <h1 class="page-title">
-      <i class="fa fa-fw fa-tag"></i> <?php echo single_cat_title(); ?>
+    <h1 class="page-title page-title-flex">
+      <span><i class="fa fa-fw fa-tag"></i> <?php echo single_cat_title(); ?></span>
+      <?php 
+      $tag = get_category( get_query_var( 'tag' ) );
+      $tag_id = $tag->ID;
+    
+      echo '<a href="' . get_tag_link( $tag_id ) . 'feed/" title="RSS de la categorie" />' . '<i class="fa fa-fw fa-rss"></i></a>' ; 
+    ?>
     </h1>
     
     <?php include(TEMPLATEPATH . '/components/posts-list.php'); ?>
