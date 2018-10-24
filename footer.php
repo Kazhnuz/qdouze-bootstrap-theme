45258a0cd6f0337e774efe89aa7293ffac2e5b4a
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:21:03 +0200

    Load jquery and bootstrap.min.js from theme
    Avoid tracking

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
   <?php include(TEMPLATEPATH . '/components/navbar-footer.php'); ?>
   </div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <script src="<?php echo get_template_directory_uri();?>/js/jquery.min.js"></script>
+  <script src="<?php echo get_template_directory_uri();?>/js/bootstrap.min.js"></script>
 </body>
 </html>
