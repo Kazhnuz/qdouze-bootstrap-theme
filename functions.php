83595b965a8f052fa689e1e3da590e94a9482501
Kazhnuz <antoine@sortilege.net>
2018-10-24 14:56:16 +0200

    some style nitpick

--- functions.php
+++ functions.php
@@ -10,10 +10,10 @@
 
   if ( function_exists('register_sidebar') )
     register_sidebar(array(
-        'before_widget' => '<section id="%1$s" class="widget %2$s card card-secondary">',
-        'after_widget' => '</section>',
-        'before_title' => '<h1 class="section card-header">',
-        'after_title' => '</h1>',
+      'before_widget' => '<section id="%1$s" class="widget %2$s card card-secondary">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h1 class="section card-header">',
+      'after_title'   => '</h1>',
     ));
     
 
