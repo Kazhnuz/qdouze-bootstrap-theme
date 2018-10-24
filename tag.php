16c3a52f5c35e313ff9d0e0ae17f370b6840b2ad
Kazhnuz <antoine@sortilege.net>
2018-10-24 18:04:36 +0200

    tags: initial tag template

---
+++ tag.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?> <!-- ouvrir header,php -->
+<main class="col-md-8">
+    <h1 class="page-title">
+      <i class="fa fa-fw fa-tag"></i> <?php echo single_cat_title(); ?>
+    </h1>
+    
+    <?php include(TEMPLATEPATH . '/components/posts-list.php'); ?>
+</main>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
