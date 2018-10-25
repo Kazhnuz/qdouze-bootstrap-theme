9e599f931174f47b95a750d6b97a3ea8571fea5d
Kazhnuz <antoine@sortilege.net>
2018-10-25 10:49:25 +0200

    sidebar: ecapsulate the widgets header
    Fix sidebar header having Teko as their font instead of Open Sans

--- functions.php
+++ functions.php
@@ -12,8 +12,8 @@
     register_sidebar(array(
       'before_widget' => '<section id="%1$s" class="widget %2$s card card-secondary">',
       'after_widget'  => '</section>',
-      'before_title'  => '<h1 class="section card-header">',
-      'after_title'   => '</h1>',
+      'before_title'  => '<div class="section card-header"><h1>',
+      'after_title'   => '</h1></div>',
     ));
     
 /* 2. Niveau support (for featured posts) */
