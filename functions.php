647d91c9ef15bc47e2280cbc51fed527271da032
Kazhnuz <antoine@sortilege.net>
2018-10-25 11:13:38 +0200

    functions/social: remove googleplus
    Not open anymore, so let's remove it

--- functions.php
+++ functions.php
@@ -82,7 +82,6 @@ function crunchify_social_sharing_buttons($content) {
 		$content .= '<a class="btn btn-twitter" href="'. $twitterURL .'" target="_blank"><i alt="twitter" class="fa fa-fw fa-twitter"></i></a>';
 		$content .= '<a class="btn btn-facebook" href="'.$facebookURL.'" target="_blank"><i alt="facebook" class="fa fa-fw fa-facebook"></i></a>';
 		$content .= '<a class="btn btn-whatsapp" href="'.$whatsappURL.'" target="_blank"><i alt="whatsapp" class="fa fa-fw fa-whatsapp"></i></a>';
-		$content .= '<a class="btn btn-googleplus" href="'.$googleURL.'" target="_blank"><i alt="google+" class="fa fa-fw fa-googleplus"></i></a>';
 		$content .= '<a class="btn btn-buffer" href="'.$bufferURL.'" target="_blank"><i alt="buffer" class="fa fa-fw fa-buffer"></i></a>';
 		$content .= '<a class="btn btn-linkedin" href="'.$linkedInURL.'" target="_blank"><i alt="linkedin" class="fa fa-fw fa-linkedin"></i></a>';
 		$content .= '<a class="btn btn-pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank"><i alt="Pin It" class="fa fa-fw fa-pinterest"></i></a>';
