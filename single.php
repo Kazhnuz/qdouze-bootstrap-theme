cb47fea0d3bd15f293c30ddf569dd7a3e6d933e2
Kazhnuz <antoine@sortilege.net>
2018-10-25 18:25:31 +0200

    article: add author/date for articles

--- single.php
+++ single.php
@@ -7,6 +7,12 @@
   
   <aside class="article-metadata">
     <div class="article-thumbnail" ><?php the_post_thumbnail(); ?></div>
+    <aside class="card card-darker">
+      <div class="media card-meta">
+	      <div class="media-left">  <? echo get_avatar( get_the_author_meta('user_email'), $size = '50'); ?></div>
+	      <div class="media-body"><author class="media-heading">Écrit par <?php the_author() ?></author><time> Le <?php the_time('l j F Y à H:i') ?> </time></div>
+      </div>
+    </aside>
     <div class="article-excerpt"><?php the_excerpt(); ?></div>
     
     <section class="article-taxonomies">
