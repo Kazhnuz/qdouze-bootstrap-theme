d664f0461febda7d456858e8ed68e9c9fec6b0b6
Kazhnuz <antoine@sortilege.net>
2018-10-27 14:29:52 +0200

    functions: indicate thumbnail support

--- functions.php
+++ functions.php
@@ -41,10 +41,11 @@ register_taxonomy(
 
 register_taxonomy_for_object_type( 'niveau', 'post' );
 
-/* 3. Excerpt support */
+/* 3. Excerpt and thumbnail support */
 
 function wpqdouze_post_supports() {
 	add_post_type_support( 'post', 'excerpt');
+	add_post_type_support( 'post', 'thumbnail');
 }
 
 add_action( 'init', 'wpqdouze_post_supports' );
